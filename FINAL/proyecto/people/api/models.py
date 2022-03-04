83180ed166bf41416032eb7341f794636578cfc7
militomba <63303365+militomba@users.noreply.github.com>
2022-03-04 14:25:46 -0300

    Delete FINAL/proyecto directory

--- FINAL/proyecto/people/api/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.conf import settings
-
-# Create your models here.
-class DatosUsuarios(models.Model):
-    nombre = models.CharField(max_length=100)
-    apellido = models.CharField(max_length=100)
-    edad = models.CharField(max_length=3)
-    
--- FINAL/proyecto/people/api/urls.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import routers
-from .views import UsuariosViewSets
-
-router = routers.SimpleRouter()
-router.register(r'persona', UsuariosViewSets, basename='persona')
-urlpatterns = router.urls
